9a124e2ce62cde28baa9f94587198bba1a2af510
Jaber Elferkh <elferkhjaber@gmail.com>
2022-05-12 13:16:20 +0100

    fix sidebar issue & add not found page & change images

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,10 +1,23 @@
+import NotFoundIllustration from 'assets/emptyStates/NotFoundIllustration';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './NotFound.module.scss';
 
 const NotFound = () => {
   return (
     <div className={styles.notFound}>
-      <h1>NotFound Page</h1>
+      <NotFoundIllustration />
+
+      <div>
+        <h1>Whooops! Lost in Space?</h1>
+        <p>
+          This page you're looking for isn't found :( <br /> We suggest you back to home
+        </p>
+
+        <Link to={'/'} className={styles.link}>
+          Back to home
+        </Link>
+      </div>
     </div>
   );
 };
